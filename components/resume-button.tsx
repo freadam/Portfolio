51277bae965aa8f15f7146f5cfc8405be50e9327
fread <freadamabebe@gmail.com>
2025-06-30 16:09:07 +0300

    update resume link

--- components/resume-button.tsx
+++ components/resume-button.tsx
@@ -5,9 +5,9 @@ import { Button } from "@/components/ui/button"
 
 export function ResumeButton() {
   return (
-    <Button 
-      size="sm" 
-      onClick={() => window.open('https://rxresu.me/freadamabebe/freadam-resume-2024', '_blank')}
+    <Button
+      size="sm"
+      onClick={() => window.open('https://rxresu.me/freadamabebe/freadam-abebe-resume-2025', '_blank')}
     >
       <ArrowUpRight className="mr-2 h-4 w-4" />
       Resume
